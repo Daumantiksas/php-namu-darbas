4e6919a11a95e1752e89af731943c5db4bb05cfa
D.Stankevicius <91727249+Daumantiksas@users.noreply.github.com>
2022-01-16 13:37:09 +0200

    vis dar neveikia

---
+++ data/flightsFrom.php
@@ -0,0 +1,21 @@
+<?php 
+$flightsFrom = [
+    [
+        'nr'=>'KN1234',
+        'town'=> 'Kaunas',
+        'price'=> rand(20, 150)
+    ],
+    [
+        'nr'=>'VN2345',
+        'town'=> 'Vilnius',
+        'price'=> rand(20, 150)
+    ],
+    [
+        'nr'=>'PN3456',
+        'town'=> 'Palanga',
+        'price'=> rand(20, 150)
+    ]
+]
+
+
+?>
---
+++ data/flightsTo.php
@@ -0,0 +1,32 @@
+<?php 
+$flightsTo = [
+    [
+        'nr'=>'BS1234',
+        'town'=> 'Bristol',
+        'price'=> rand(20, 150)
+    ],
+    [
+        'nr'=>'LN2345',
+        'town'=> 'London',
+        'price'=> rand(20, 150)
+    ],
+    [
+        'nr'=>'WS3456',
+        'town'=> 'Warshaw',
+        'price'=> rand(20, 150)
+    ],
+    [
+        'nr'=>'RY4567',
+        'town'=> 'Riga',
+        'price'=> rand(20, 150)
+    ],
+    [
+        'nr'=>'TL5678',
+        'town'=> 'Tallinn',
+        'price'=> rand(20, 150)
+    ]
+
+]
+
+
+?>
